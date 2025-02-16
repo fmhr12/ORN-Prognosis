97c47275813ed9a253958fb25afc29b24184ad52
Faraz <158780270+fmhr12@users.noreply.github.com>
2025-02-16 14:30:42 -0500

    Update app.R

--- app.R
+++ app.R
@@ -296,6 +296,7 @@ server <- function(input, output, session) {
 if (interactive()) {
   shinyApp(ui = ui, server = server)
 } else {
+  # In a production (non-interactive) environment, use the PORT provided by Render.
   shiny::runApp(list(ui = ui, server = server), 
                 host = "0.0.0.0", 
                 port = as.numeric(Sys.getenv("PORT", 10000)))
